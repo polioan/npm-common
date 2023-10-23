aa6c361ab192d08cebdbec5327a39ae16becd96b
polioan <rognash.yekan@yandex.ru>
2023-10-23 09:56:41 +0300

    add noop

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './runtime/sleep'
 export * from './runtime/isMobile'
 export * from './runtime/randomChoice'
+export * from './runtime/noop'
 
 export * from './types/enumof'
 export * from './types/falsy'
