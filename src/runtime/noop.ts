024afa903b996ca361ec9bf74f90bb673c406166
polioan <rognash.yekan@yandex.ru>
2024-04-15 11:38:20 +0300

    add async noop

--- src/runtime/noop.ts
+++ src/runtime/noop.ts
@@ -1,2 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export function noop() {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export async function anoop() {}
